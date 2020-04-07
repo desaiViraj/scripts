94f280d99f5c679f7be5334ea08dd2418198a487
desaiViraj <desaiviraj@yahoo.com>
2020-04-07 16:25:59 +0000

    array oprations

---
+++ array.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+array=(one two three four 11 22 33 44)
+echo "-----------------------------------------------------------"
+echo "List of arrays using index value:"
+echo "1st=${array[0]}"
+echo "2nd=${array[1]}"
+echo "3rd=${array[2]}"
+echo "4th=${array[3]}"
+echo "5th=${array[4]}"
+echo "6th=${array[5]}"
+echo "7th=${array[6]}"
+echo "8th=${array[7]}"
+echo "-----------------------------------------------------------"
+echo "List all elements of array using # and *"
+echo "Using @: ${array[@]}"
+echo "Using *: ${array[*]}"
+echo "-----------------------------------------------------------"
+echo "List the number of elements in array"
+echo "Total elements= ${#array[*]}"
+echo "-----------------------------------------------------------"
+echo "List all values of array using for loop"
+for i  in ${!array[@]}
+do
+      echo "value at index $i is ${array[$i]}"
+done
+echo "-----------------------------------------------------------"
+echo "List index values"
+echo "${!array[*]}"
+echo "-----------------------------------------------------------"
