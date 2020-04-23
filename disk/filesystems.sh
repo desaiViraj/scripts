11324ccb8e5eca595fa70ca31be69e3c82df7930
desaiViraj <desaiviraj@yahoo.com>
2020-04-23 18:32:19 +0000

    Info on filesystem

---
+++ disk/filesystems.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "*************************FILESYSTEMS**************************"
+echo "FILESYSTEM: It is a way of organizing files on the partitions."
+echo "#cat /proc/filesystems; man fs"
+echo "-----------------------COMMON FS------------------------------"
+echo ""
