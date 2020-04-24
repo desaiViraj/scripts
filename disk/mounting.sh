bfd6fb5408745584ad6545cbbbdc62dbf9a2abe9
desaiViraj <desaiviraj@yahoo.com>
2020-04-24 17:33:32 +0000

    Info on Mounting File Systems

---
+++ disk/mounting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "____________________________________________________________________________"
+echo "-----------------------------MOUNTING---------------------------------------"
+echo "Mounting a file system makes it available for the use."
+echo "To mount use, mount command
+Example: mount -t /dev/sda1"
+
